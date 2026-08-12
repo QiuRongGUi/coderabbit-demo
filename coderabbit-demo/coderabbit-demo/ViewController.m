675b8c9b95774df9e2715e47f1c873bd278fe6ac
admin <qiuronggui@01.ai>
2026-08-12 18:07:01 +0800

    add .coderabbit.yaml

--- coderabbit-demo/coderabbit-demo/ViewController.m
+++ coderabbit-demo/coderabbit-demo/ViewController.m
@@ -19,43 +19,11 @@
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     
-    [self shaow];
-    
     
     
     NSString *name = nil;
     NSLog(@"%@", name.length);
 }
 
-- (void)shaow {
-    
-}
-
-- (void)shaow1 {
- 
-    dispatch_async(dispatch_get_global_queue(0,0), ^{
-        self.label.text = @"123";
-    });
-}
-
-- (void)shaow2 {
-    
-}
-
-- (void)shaow3 {
-    
-}
-
-- (void)shaow4 {
-    
-}
-
-- (void)shaow5 {
-    
-}
-
-- (void)shaow6 {
-    
-}
 
 @end
